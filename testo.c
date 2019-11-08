311ba281c8d892943ef908d60e2bc46f5c97f27f
DrMrMan <57523040+DrMrMan@users.noreply.github.com>
2019-11-08 11:04:43 +0200

    Add files via upload
    changed father to mother

--- testo.c
+++ testo.c
@@ -5,7 +5,7 @@ int main()
 	int i;
 	printf("please enter a num: \n");
 	scanf("%d", &i);
-	printf("The num u entered daddy is: %d\n", i);
+	printf("The num u entered mother is: %d\n", i);
 
 	return 0;
 }
